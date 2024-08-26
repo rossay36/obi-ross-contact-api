f59fede736896cc5cec5325726711247a5049811
Ross <obi75309@gmail.com>
2024-08-27 04:23:27 +1300

    adding my frontend url

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,8 @@
 const allowedOrigins = [
 	"http://localhost:5173",
 	"http://localhost:3000",
-	"https://firebasestorage.googleapis.com",
+	"https://obi-ross-potfolio.onrender.com",
+	"https://ross-portfolio.onrender.com",
 ];
 
 module.exports = allowedOrigins;
